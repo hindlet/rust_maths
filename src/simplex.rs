36b72a72c51172e0544a63db69bb68f91aebfc64
hindlet <79304662+hindlet@users.noreply.github.com>
2023-07-12 10:12:30 +0100

    moved noise to file and added 3d noise

--- src/simplex.rs
+++ src/simplex.rs
@@ -19,15 +19,6 @@ pub fn grad3(index: i32) -> Vector3{
     }
 }
 
-// fn grad3(hash: u32, x: f32, y: f32) -> f32{
-//     let h = hash & 0x3F;
-//     let u = if h < 4 {x} else {y};
-//     let v = if h < 4 {y} else {x};
-//     let mut out = if h & 1 == 1 {-u} else {u};
-//     if h & 2 == 2 {out += -2.0 * v} else {out += 2.0 * v};
-//     out
-// }
-
 const GRAD4: [Vector4; 32] = [
     Vector4{x: 0.0, y: 1.0, z: 1.0, w: 1.0},
     Vector4{x: 0.0, y: 1.0, z: 1.0, w: -1.0},
@@ -118,7 +109,7 @@ fn hash(hash: i32) -> i32 {
     PERM[hash as usize]
 }
 
-
+/// 2d simplex noise function, takes in an x and y value and outputs a value between -1 and 1
 pub fn simplex2d(x: f32, y: f32) -> f32 {
     let (n0, n1, n2): (f32, f32, f32);
 
@@ -187,5 +178,98 @@ pub fn simplex2d(x: f32, y: f32) -> f32 {
         n2 = t2 * t2 * grad3(gi2).xy().dot([x2, y2]);
     }
 
-    (45.23065 * (n0 + n1 + n2) + 1.0) / 2.0
+    45.23065 * (n0 + n1 + n2)
+}
+
+/// 3d simplex noise function, takes in an x, y and z value and outputs a value between -1 and 1
+pub fn simplex3d(x: f32, y: f32, z: f32) -> f32 {
+    let (n0, n1, n2, n3): (f32, f32, f32, f32);
+
+    let skew = skew_val(3);
+    let unskew = unskew_val(3);
+
+    // skew for simplex cell coords
+    let s = (x + y + z) * skew;
+    let i = (x + s).floor() as i32;
+    let j = (y + s).floor() as i32;
+    let k = (z + s).floor() as i32;
+
+    // unskew for for 3d space coords
+    let t = (i + j + k) as f32 * unskew;
+    let x0_1 = i as f32 - t;
+    let y0_1 = j as f32 - t;
+    let z0_1 = k as f32 - t;
+    let x0 = x - x0_1;
+    let y0 = y - y0_1;
+    let z0 = z - z0_1;
+
+    // figure out which simplex tetrahedron we're in
+    let (i1, j1, k1, i2, j2, k2) = if x0 >= y0 {
+        if y0 >= z0 { // x y z
+            (1, 0, 0, 1, 1, 0)
+        } else if x0 >= z0 { // x z y
+            (1, 0, 0, 1, 0 ,1)
+        } else { // z x y
+            (0, 0, 1, 1, 0, 1)
+        }
+    } else {
+        if y0 < z0 { // z y x
+            (0, 0, 1, 0, 1, 1)
+        } else if x0 < z0 { // y z x
+            (0, 1, 0, 0, 1, 1)
+        } else { // y x z
+            (0, 1, 0, 1, 1, 0)
+        }
+    };
+
+    let x1 = x0 - i1 as f32 + unskew; // second corner corner
+    let y1 = y0 - j1 as f32 + unskew;
+    let z1 = z0 - k1 as f32 + unskew;
+    let x2 = x0 - i2 as f32 + 2.0 * unskew; // third corner
+    let y2 = y0 - j2 as f32 + 2.0 * unskew;
+    let z2 = z0 - k2 as f32 + 2.0 * unskew;
+    let x3 = x0 - 1.0 + 3.0 * unskew; // last corner
+    let y3 = y0 - 1.0 + 3.0 * unskew;
+    let z3 = z0 - 1.0 + 3.0 * unskew;
+
+    // hased indices
+    let ii = i & 255;
+    let jj = j & 255;
+    let kk = k & 255;
+    let gi0 = hash(ii + hash(jj + hash(kk)));
+    let gi1 = hash(ii + i1 + hash(jj + j1 + hash(kk + k1)));
+    let gi2 = hash(ii + i2 + hash(jj + j2 + hash(kk + k2)));
+    let gi3 = hash(ii + 1 + hash(jj + 1 + hash(kk + 1)));
+
+    // calculate corner contribution
+    let mut t0 = 0.5 - x0 * x0 - y0 * y0 - z0 * z0;
+    if t0 < 0.0 {n0 = 0.0}
+    else {
+        t0 *= t0;
+        n0 = t0 * t0 * grad3(gi0).dot([x0, y0, z0]);
+    }
+
+    let mut t1 = 0.5 - x1 * x1 - y1 * y1 - z1 * z1;
+    if t1 < 0.0 {n1 = 0.0}
+    else {
+        t1 *= t1;
+        n1 = t1 * t1 * grad3(gi1).dot([x1, y1, z1]);
+    }
+
+    let mut t2 = 0.5 - x2 * x2 - y2 * y2 - z2 * z2;
+    if t2 < 0.0 {n2 = 0.0}
+    else {
+        t2 *= t2;
+        n2 = t2 * t2 * grad3(gi2).dot([x2, y2, z2]);
+    }
+
+    let mut t3 = 0.5 - x3 * x3 - y3 * y3 - z3 * z3;
+    if t3 < 0.0 {n3 = 0.0}
+    else {
+        t3 *= t3;
+        n3 = t3 * t3 * grad3(gi3).dot([x3, y3, z3]);
+    }
+
+
+    32.0 * (n0 + n1 + n2 + n3)
 }
