2f19c3e5fc6483ea8e393121db4676e374d30de8
hindlet <thomas@familyhindle.net>
2024-12-27 21:48:27 +0000

    camera maths moved from graphics crate

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,9 @@ pub use noise::*;
 // interpolation
 mod interpolation;
 pub use interpolation::*;
+// camera maths
+mod camera;
+pub use camera::Camera;
 
 use std::f32::consts::{FRAC_2_SQRT_PI, SQRT_2, PI};
 pub use rand_chacha::{ChaChaRng, rand_core::SeedableRng};
